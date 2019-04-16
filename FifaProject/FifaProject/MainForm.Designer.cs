2410b4c71a9f974005fbad6d9878c29b91211fe4
NotUneay <neallhaek@hotmail.com>
2019-04-16 11:01:58 +0200

    c# update

--- FifaProject/FifaProject/MainForm.Designer.cs
+++ FifaProject/FifaProject/MainForm.Designer.cs
@@ -28,13 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.teamLabel8 = new System.Windows.Forms.Label();
+            this.teamLabel7 = new System.Windows.Forms.Label();
+            this.teamLabel6 = new System.Windows.Forms.Label();
+            this.teamLabel5 = new System.Windows.Forms.Label();
+            this.teamLabel4 = new System.Windows.Forms.Label();
+            this.teamLabel3 = new System.Windows.Forms.Label();
+            this.teamLabel2 = new System.Windows.Forms.Label();
+            this.teamLabel1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gokkers";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.teamLabel8);
+            this.groupBox1.Controls.Add(this.teamLabel7);
+            this.groupBox1.Controls.Add(this.teamLabel6);
+            this.groupBox1.Controls.Add(this.teamLabel5);
+            this.groupBox1.Controls.Add(this.teamLabel4);
+            this.groupBox1.Controls.Add(this.teamLabel3);
+            this.groupBox1.Controls.Add(this.teamLabel2);
+            this.groupBox1.Controls.Add(this.teamLabel1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(221, 360);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // teamLabel8
+            // 
+            this.teamLabel8.AutoSize = true;
+            this.teamLabel8.Location = new System.Drawing.Point(135, 181);
+            this.teamLabel8.Name = "teamLabel8";
+            this.teamLabel8.Size = new System.Drawing.Size(62, 13);
+            this.teamLabel8.TabIndex = 10;
+            this.teamLabel8.Text = "teamLabel8";
+            // 
+            // teamLabel7
+            // 
+            this.teamLabel7.AutoSize = true;
+            this.teamLabel7.Location = new System.Drawing.Point(3, 181);
+            this.teamLabel7.Name = "teamLabel7";
+            this.teamLabel7.Size = new System.Drawing.Size(62, 13);
+            this.teamLabel7.TabIndex = 9;
+            this.teamLabel7.Text = "teamLabel7";
+            // 
+            // teamLabel6
+            // 
+            this.teamLabel6.AutoSize = true;
+            this.teamLabel6.Location = new System.Drawing.Point(135, 136);
+            this.teamLabel6.Name = "teamLabel6";
+            this.teamLabel6.Size = new System.Drawing.Size(62, 13);
+            this.teamLabel6.TabIndex = 8;
+            this.teamLabel6.Text = "teamLabel6";
+            // 
+            // teamLabel5
+            // 
+            this.teamLabel5.AutoSize = true;
+            this.teamLabel5.Location = new System.Drawing.Point(3, 136);
+            this.teamLabel5.Name = "teamLabel5";
+            this.teamLabel5.Size = new System.Drawing.Size(62, 13);
+            this.teamLabel5.TabIndex = 7;
+            this.teamLabel5.Text = "teamLabel5";
+            // 
+            // teamLabel4
+            // 
+            this.teamLabel4.AutoSize = true;
+            this.teamLabel4.Location = new System.Drawing.Point(135, 91);
+            this.teamLabel4.Name = "teamLabel4";
+            this.teamLabel4.Size = new System.Drawing.Size(62, 13);
+            this.teamLabel4.TabIndex = 6;
+            this.teamLabel4.Text = "teamLabel4";
+            // 
+            // teamLabel3
+            // 
+            this.teamLabel3.AutoSize = true;
+            this.teamLabel3.Location = new System.Drawing.Point(3, 91);
+            this.teamLabel3.Name = "teamLabel3";
+            this.teamLabel3.Size = new System.Drawing.Size(62, 13);
+            this.teamLabel3.TabIndex = 5;
+            this.teamLabel3.Text = "teamLabel3";
+            // 
+            // teamLabel2
+            // 
+            this.teamLabel2.AutoSize = true;
+            this.teamLabel2.Location = new System.Drawing.Point(135, 44);
+            this.teamLabel2.Name = "teamLabel2";
+            this.teamLabel2.Size = new System.Drawing.Size(62, 13);
+            this.teamLabel2.TabIndex = 4;
+            this.teamLabel2.Text = "teamLabel2";
+            // 
+            // teamLabel1
+            // 
+            this.teamLabel1.AutoSize = true;
+            this.teamLabel1.Location = new System.Drawing.Point(3, 44);
+            this.teamLabel1.Name = "teamLabel1";
+            this.teamLabel1.Size = new System.Drawing.Size(62, 13);
+            this.teamLabel1.TabIndex = 3;
+            this.teamLabel1.Text = "teamLabel1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(245, 423);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label teamLabel8;
+        private System.Windows.Forms.Label teamLabel7;
+        private System.Windows.Forms.Label teamLabel6;
+        private System.Windows.Forms.Label teamLabel5;
+        private System.Windows.Forms.Label teamLabel4;
+        private System.Windows.Forms.Label teamLabel3;
+        private System.Windows.Forms.Label teamLabel2;
+        private System.Windows.Forms.Label teamLabel1;
     }
 }
 
--- FifaProject/FifaProject/MainForm.cs
+++ FifaProject/FifaProject/MainForm.cs
@@ -16,5 +16,6 @@ namespace FifaProject
         {
             InitializeComponent();
         }
+
     }
 }
