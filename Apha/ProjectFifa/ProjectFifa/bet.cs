516020dac92224e8b42ac82aed38bb504a9ec7f5
NotUneay <neallhaek@hotmail.com>
2019-05-29 10:40:26 +0200

    img added

--- Apha/ProjectFifa/ProjectFifa/bet.cs
+++ Apha/ProjectFifa/ProjectFifa/bet.cs
@@ -9,7 +9,7 @@ namespace ProjectFifa
     public class bet
     {
         public string betMatch { get; set; }
-        public string Winningteam { get; set; }
+        public string winningTeam { get; set; }
         public int betAmount { get; set; }
         public bettor bettor { get; set; }
     }
